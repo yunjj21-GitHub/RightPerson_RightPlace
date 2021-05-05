d286d45faa8f5dca9f62261405105bf56f54564f
yunjj21 <yunjj21@hs.ac.kr>
2021-05-05 23:58:32 +0900

    첫화면 변경 및 Splash화면 구현, logo이미지 변경

--- app/src/main/java/org/techtown/rightperson_rightplace/login.java
+++ app/src/main/java/org/techtown/rightperson_rightplace/login.java
@@ -8,6 +8,8 @@ public class login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme); // Splash 화면 구현을 위함
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
